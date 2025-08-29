79b048e230375c361c164ee7e660c54cae9fe273
Johan Persson <johan162@gmail.com>
2025-08-29 23:44:02 +0200

    Refactor delete frame disrectory

--- src/video.rs
+++ src/video.rs
@@ -1,8 +1,7 @@
-use crate::model::SimModel;
+use crate::model::{SimModel, try_delete_frames_dir};
 use chrono::Duration;
 use colored::Colorize;
 use std::error::Error;
-use std::fs;
 use std::io::Write;
 use std::path::Path;
 use std::process::Command;
@@ -16,30 +15,10 @@ pub fn try_video_encoding(model: &mut SimModel) -> Result<Duration, Box<dyn std:
 
         try_print_encoding_info(model, encoder, &animation_file_name);
 
-        if !model.quiet {
-            println!(
-                "{}",
-                format!("Encoding video with at {} FPS ...", model.frame_rate)
-                    .color(colored::Color::Green)
-                    .bold()
-            );
-            if model.verbosity == 1 {
-                println!(
-                    "{}",
-                    format!("Running: 'ffmpeg -framerate {} -pattern_type glob -i \"{}/frame_*.png\" -c:v {} -pix_fmt yuv420p \"{}\"'", 
-                    model.frame_rate, model.frames_dir, encoder, animation_file_name).color(colored::Color::Blue).bold()
-                );
-            }
-            // Empty IO buffer to make sure the output is flushed immediately
-            std::io::stdout().flush().unwrap();
-        }
-        // Determine the time to do ffmpeg encoding.
-        let start_time = std::time::Instant::now();
-
         // Build the ffmpeg command to create the video
         // Example1 (Linux): ffmpeg -framerate 5 -pattern_type glob -i 'frames_dir/*.png' -c:v hevc_nvenc        -pix_fmt yuv420p cutter_sim.mp4
         // Example2 (MacOS): ffmpeg -framerate 5 -pattern_type glob -i 'frames_dir/*.png' -c:v hevc_videotoolbox -pix_fmt yuv420p cutter_sim.mp4
-        //
+        let start_time = std::time::Instant::now();
         let video_cmd_output = Command::new("ffmpeg")
             .args([
                 "-framerate",
@@ -50,24 +29,36 @@ pub fn try_video_encoding(model: &mut SimModel) -> Result<Duration, Box<dyn std:
                 &format!("{}/frame_*.png", model.frames_dir),
                 "-c:v",
                 encoder,
+                "-allow_sw",
+                "1",
                 "-pix_fmt",
                 "yuv420p",
                 &animation_file_name,
             ])
             .output();
-
-        // Time duration of the ffmpeg command
         duration = chrono::Duration::from_std(start_time.elapsed()).unwrap();
 
         if video_cmd_output.is_err() || !video_cmd_output.as_ref().unwrap().status.success() {
             if model.verbosity > 0 {
                 eprintln!(
                     "{} {}",
-                    "Error creating video: 'ffmpeg' command failed. Most likely the H.265/HEVC encoder is not installed or does not support the frame size.".color(colored::Color::Red).bold(),
-                    "Retrying with SW encoding fallback options.".color(colored::Color::Red).bold()
+                    "Error creating video: 'ffmpeg' command failed. Most likely the H.265/HEVC encoder is not installed or does not support the frame size.\n".color(colored::Color::Yellow).bold(),
+                    "Retrying with default encoding fallback options.".color(colored::Color::Yellow).bold()
                 );
                 std::io::stderr().flush().unwrap();
             }
+
+            if model.verbosity > 0 {
+                eprintln!(
+                    "{}",
+                    "Retrying: 'ffmpeg -framerate 5 -pattern_type glob -i '_frames_dir/*.png' -pix_fmt yuv420p cutter_sim.mp' with default encoding options without specifying the encoder."
+                        .color(colored::Color::Blue)
+                        .bold()
+                );
+                std::io::stderr().flush().unwrap();
+            }
+
+            let start_time = std::time::Instant::now();
             let video_cmd_output = Command::new("ffmpeg")
                 .args([
                     "-framerate",
@@ -76,93 +67,56 @@ pub fn try_video_encoding(model: &mut SimModel) -> Result<Duration, Box<dyn std:
                     "glob",
                     "-i",
                     &format!("{}/frame_*.png", model.frames_dir),
-                    "-c:v",
-                    encoder,
-                    "-allow_sw 1", // Allow software encoding if hardware encoding is not available for some reason
                     "-pix_fmt",
                     "yuv420p",
                     &animation_file_name,
                 ])
                 .output();
+            duration = chrono::Duration::from_std(start_time.elapsed()).unwrap();
+
             if video_cmd_output.is_err() || !video_cmd_output.as_ref().unwrap().status.success() {
-                let cmd = format!(
-                    "ffmpeg -framerate {} -pattern_type glob -i '{}/frame_*.png' -c:v {} -allow_sw 1 -pix_fmt yuv420p {}",
-                    model.frame_rate, model.frames_dir, encoder, animation_file_name
-                );
                 Err(Box::new(
                     std::io::Error::other(
-                        format!("{}\n{}",
+                        format!("{}",
                             "Error creating video: 'ffmpeg' exists, but the correct ffmpeg H.265/HEVC encoder is perhaps not installed?"
                             .color(colored::Color::Red)
-                            .bold(),
-                            cmd)
+                            .bold())
                     )
                 ))?;
             }
-        } else {
-            if !model.quiet {
-                println!(
-                    "{} {}",
-                    "Video created successfully:"
-                        .color(colored::Color::Green)
-                        .bold(),
-                    animation_file_name.color(colored::Color::Cyan).bold()
-                );
-            }
-            if model.delete_frames {
-                // Delete the entire output directory
-                if let Err(e) = fs::remove_dir_all(&model.frames_dir) {
-                    Err(Box::new(std::io::Error::other(format!(
-                        "{} '{}' : {}",
-                        "Failed to delete frames directory"
-                            .color(colored::Color::Red)
-                            .bold(),
-                        model.frames_dir.as_str(),
-                        e
-                    ))))?;
-                } else if !model.quiet && model.verbosity > 1 {
-                    println!(
-                        "{}",
-                        "Frames directory deleted successfully."
-                            .color(colored::Color::Green)
-                            .bold()
-                    );
-                }
-            } else if !model.quiet && model.verbosity > 1 {
-                println!(
-                    "{}",
-                    "Frames are kept in the output directory."
-                        .color(colored::Color::Green)
-                        .bold()
-                );
-            }
         }
+        if !model.quiet {
+            println!(
+                "{} {}",
+                "Video created successfully: "
+                    .color(colored::Color::Green)
+                    .bold(),
+                animation_file_name.color(colored::Color::Cyan).bold()
+            );
+        }
+        try_delete_frames_dir(model)?;
     }
     Ok(duration)
 }
 
 fn try_print_encoding_info(model: &SimModel, encoder: &'static str, animation_file_name: &String) {
-    if !model.quiet && model.verbosity > 1 {
+    if !model.quiet && model.verbosity > 0 {
         println!(
             "{}",
-            format!("Creating video using H.265 encoding from frames using \"{encoder}\" encoder")
-                .color(colored::Color::Green)
-                .bold()
+            format!(
+                "Creating video using H.265 with \"{encoder}\" encoder with {frame_rate} FPS",
+                frame_rate = model.frame_rate
+            )
+            .color(colored::Color::Green)
+            .bold()
         );
-        println!(
-                "{} {}",
-                "Running command:".color(colored::Color::Green).bold(),
-                format!(
-                    "ffmpeg -framerate {} -pattern_type glob -i '{}/frame_*.png' -c:v {} -allow_sw 1 -pix_fmt yuv420p {}",
-                    model.frame_rate,
-                    model.frames_dir,
-                    encoder,
-                    animation_file_name
-                )
-                .color(colored::Color::Cyan)
-                .bold()
+        if model.verbosity > 1 {
+            println!(
+            "{}",
+            format!("ffmpeg -framerate {} -pattern_type glob -i \"{}/frame_*.png\" -c:v {} -allow_sw 1 -pix_fmt yuv420p \"{}\"", 
+                model.frame_rate, model.frames_dir, encoder, animation_file_name).color(colored::Color::Blue).bold()
             );
-        // Empty IO buffer to make sure the output is flushed immediately
+        }
         std::io::stdout().flush().unwrap();
     }
 }
